c96e7ee9b49cd27866831bb9fc5ca79e9680fd3a
aovs_28 <aovsepy28@gmail.com>
2023-10-03 00:20:17 -0700

    Created the option to switch between login and sign up pages.

---
+++ app/src/main/java/com/example/smarthomie/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.smarthomie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        TextView btn = findViewById(R.id.RegisterText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smarthomie/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.smarthomie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView btn = findViewById(R.id.LoginText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+
+        });
+    }
+}
