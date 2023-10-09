2b77d2e4788c37cc5b4b34418f50d85d5295a827
aovs_28 <aovsepy28@gmail.com>
2023-10-08 22:38:05 -0700

    Added a my_devices.xml page and fixed the login in and sign up page to go to the my_devices.xml page when the user registers or logs in.

--- app/src/main/java/com/example/smarthomie/LoginActivity.java
+++ app/src/main/java/com/example/smarthomie/LoginActivity.java
@@ -5,22 +5,32 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.util.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         TextView btn = findViewById(R.id.RegisterText);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        Button but = (Button)findViewById(R.id.loginButton);
+
+        btn.setOnClickListener(this);
+        but.setOnClickListener(this);
+
+
             }
-        });
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.RegisterText) {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        } else if (id == R.id.loginButton) {
+            startActivity(new Intent(LoginActivity.this, MyDevices.class));
+        }
     }
 }
---
+++ app/src/main/java/com/example/smarthomie/MyDevices.java
@@ -0,0 +1,20 @@
+package com.example.smarthomie;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyDevices extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_devices);
+    }
+}
