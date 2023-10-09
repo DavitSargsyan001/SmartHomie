8c5dfd1899d531430bc1e97fe23e08b7673283b4
Davit Sargsyan <davit.sargsyan.603@my.csun.edu>
2023-10-09 11:37:08 -0700

    "Added some changes, work in progress"

--- app/src/main/java/com/example/smarthomie/RegisterActivity.java
+++ app/src/main/java/com/example/smarthomie/RegisterActivity.java
@@ -3,13 +3,25 @@ package com.example.smarthomie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.AuthResult;
+
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText email;
+    private EditText password;
+    private Button register;
+
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +32,45 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         btn.setOnClickListener(this);
         but.setOnClickListener(this);
 
+        email = findViewById(R.id.emailInput);
+        password =  findViewById(R.id.passwordInput);
+        register = findViewById(R.id.registerButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty( txt_email) || TextUtils.isEmpty(txt_password)){
+                    Toast.makeText( RegisterActivity.this, "Empty crcedentials!", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText( RegisterActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
+                } else{
+                    registerUser(txt_email , txt_password);
+                }
+
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this ,new onCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText( RegisterActivity.this, "Registering user successfull" , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText( RegisterActivity.this, "Registering failed!" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
 
     }
+
 
     @Override
     public void onClick(View v) {
@@ -33,4 +82,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             startActivity(new Intent(RegisterActivity.this, MyDevices.class));
         }
     }
+
+
+
 }
