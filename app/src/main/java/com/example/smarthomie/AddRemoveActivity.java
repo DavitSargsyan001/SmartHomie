120803af0b2212c6e6f99796ef5049240ac96c5e
Davit <sargsyan_davit@protonmail.com>
2023-10-11 22:56:07 -0700

    "Added and modified some files"

---
+++ app/src/main/java/com/example/smarthomie/AddRemoveActivity.java
@@ -0,0 +1,11 @@
+package com.example.smarthomie;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class AddRemoveActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_remove_device);
+    }
+}
--- app/src/main/java/com/example/smarthomie/homePage.java
+++ app/src/main/java/com/example/smarthomie/homePage.java
@@ -2,31 +2,37 @@ package com.example.smarthomie;
 
 import android.os.Bundle;
 import android.content.Intent;
-import android.text.TextUtils;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import java.util.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class homePage extends AppCompatActivity implements View.OnClickListener {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.home_page);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
 
-            Button scenarioButton = (Button)findViewById(R.id.button2);
-            scenarioButton.setOnClickListener(this);
+        Button scenarioButton = (Button)findViewById(R.id.button2);
+        scenarioButton.setOnClickListener(this);
+
+        // Step 1: Find the button and set the click listener
+        Button addRemoveButton = (Button)findViewById(R.id.button);
+        addRemoveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button2) {
+            startActivity(new Intent(homePage.this, Scenarios.class));
         }
 
-        @Override
-        public void onClick(View v) {
-            int id = v.getId();
-            if (id == R.id.button2) {
-                startActivity(new Intent(homePage.this, Scenarios.class));
-            }
+        // Step 2: Handle the button click to start the desired activity
+        if (id == R.id.button) {
+            startActivity(new Intent(homePage.this, AddRemoveActivity.class));
         }
     }
+}
+
 
