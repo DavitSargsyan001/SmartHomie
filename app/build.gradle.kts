1e14bdd7c24c64bcde4fe530ecc0d2f0ff9b3691
aovs_28 <aovsepy28@gmail.com>
2023-10-14 13:42:43 -0700

    Added a home button to add remove page and created a my devices section on home page.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.firebase:firebase-auth:22.1.2")
+    implementation("com.google.firebase:firebase-firestore:24.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/smarthomie/AddRemoveActivity.java
+++ app/src/main/java/com/example/smarthomie/AddRemoveActivity.java
@@ -1,11 +1,25 @@
 package com.example.smarthomie;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 public class AddRemoveActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_remove_device);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.ibHome2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddRemoveActivity.this, homePage.class));
+            }
+        });
     }
 }
