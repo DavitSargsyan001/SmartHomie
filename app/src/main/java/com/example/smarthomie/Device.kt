d4b1bab600d7cd004531ec97d33fdf549ff7a7f0
Davit <sargsyan_davit@protonmail.com>
2023-10-24 23:21:27 -0700

    "Further developed Device class, made a Devices collection which would eventually store all the added devices and plus made updateStatus function which should handle the status updated of devices"

--- app/src/main/java/com/example/smarthomie/Device.kt
+++ app/src/main/java/com/example/smarthomie/Device.kt
@@ -1,14 +1,77 @@
 package com.example.smarthomie
 
+import com.example.smarthomie.DeviceStatus
+
 class Device {
 
-        val id: Int = 0                // Immutable
+        //val id: Int = 0                // Immutable
         var name: String = ""            // Mutable property
-        var type: String = ""
-        var status: String = ""
+        var type: DeviceType? = null
+        var status: DeviceStatus? = null
         var icon: String = ""
-        val ownerUserId: Int = 0         // Assuming owner won't change
-        var lastUpdated: Long = 0L
+        //val ownerUserId: Int = 0         // Assuming owner won't change
+        //var lastUpdated: Long = 0L
+
+        enum class DeviceType(val displayName: String) {
+                LIGHTBULB("Light Bulb"),
+                SMART_PLUG("Smart Plug"),
+                THERMOSTAT("Thermostat"),
+                CAMERA("Camera"),
+                SENSOR("Sensor"),
+                COFFEEMACHINE("Coffee Machine");
+        }
+
+        fun updateStatus(newStatus: DeviceStatus){
+                when (type) {
+                        DeviceType.LIGHTBULB -> {
+                                if (newStatus is LightBulbStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a light bulb")
+                                }
+                        }
+                        DeviceType.SMART_PLUG -> {
+                                if (newStatus is SmartPlugStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a smart plug")
+                                }
+                        }
+                        DeviceType.CAMERA -> {
+                                if (newStatus is CameraStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a camera")
+                                }
+                        }
+                        DeviceType.THERMOSTAT -> {
+                                if (newStatus is ThermostatStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a thermostat")
+                                }
+                        }
+                        DeviceType.SENSOR -> {
+                                if (newStatus is SensorStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a sensor")
+                                }
+                        }
+                        DeviceType.COFFEEMACHINE -> {
+                                if (newStatus is CoffeeMachineStatus) {
+                                        status = newStatus
+                                } else {
+                                        throw IllegalArgumentException("Invalid status for a coffee machine")
+                                }
+                        }
+                        else -> {
+                                throw IllegalArgumentException("Unknown device type")
+                        }
+                }
+        }
+
+
 
 
 }
