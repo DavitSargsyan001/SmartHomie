a9e360a87e536de02f40f875fe9f6b3dd0d37299
aovs_28 <aovsepy28@gmail.com>
2023-11-14 18:44:04 -0800

    Added the ability to control smart plug from app, but it only powers it on.

--- app/src/main/java/com/example/smarthomie/MyDevices.java
+++ app/src/main/java/com/example/smarthomie/MyDevices.java
@@ -1,36 +1,55 @@
 package com.example.smarthomie;
 
-import static com.example.smarthomie.R.id.ibHome3;
+
 
 import android.os.Bundle;
 import android.content.Intent;
-import android.text.TextUtils;
-import android.util.Patterns;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import java.util.*;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
+
 public class MyDevices extends AppCompatActivity {
     ImageButton myImageButton;
+    Button but;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_devices);
 
         myImageButton = (ImageButton) findViewById(R.id.ibHome3);
-
+        but = (Button)findViewById(R.id.button7);
         myImageButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent goToHomePage = new Intent(MyDevices.this, homePage.class);
                 startActivity(goToHomePage);
             }
         });
+
+        but.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PhilipsHueController.turnOnLight(PhilipsHueController.createTrustAllSSLContext());
+                    }
+                });
+                thread.start();
+            }
+        });
+
     }
-
-
 }
