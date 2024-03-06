4e971c79b1e408178340dd1c095db00642340811
aovs_28 <aovsepy28@gmail.com>
2024-03-05 19:57:22 -0800

    Fixed the Profile Page buttons to work

--- app/src/main/java/com/example/smarthomie/Profile.java
+++ app/src/main/java/com/example/smarthomie/Profile.java
@@ -1,6 +1,11 @@
 package com.example.smarthomie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
@@ -8,5 +13,24 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
+
+        ImageButton myImageButton = (ImageButton) findViewById(R.id.ibHome2);
+        Button logOutButton = (Button) findViewById(R.id.logout);
+
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToHomePage = new Intent(Profile.this, homePage.class);
+                startActivity(goToHomePage);
+            }
+        });
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logOut = new Intent(Profile.this, LoginActivity.class);
+                startActivity(logOut);
+            }
+        });
     }
 }
